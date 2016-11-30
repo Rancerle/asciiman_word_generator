7a7e049796809fc43f2f0eadc7fab875ab8dd2a7
Rancerle <reillycurran880@gmail.com>
2016-11-29 22:52:22 -0500

    Add files via upload
    fixes several issues that existed in previous versions, will soon include ascii character drawings

--- hangman.cpp
+++ hangman.cpp
@@ -1,34 +1,46 @@
 //Created by: Reilly Q. Curran
-//last edited: 11/23/2016
-//last compiled: 11/23/2016
+//last edited: 11/29/2016
+//last compiled: 11/29/2016
 
 #include<iostream>
 #include<string>
+//couldn't decide what randomizer was better
 #include <stdlib.h>
 #include <time.h>
 
+//allows the word and hint to be indexed in the same place
 struct randWord
 {
 	std::string word;
 	std::string hint;
 };
 
+//creates an empty array of words for the program to initialize
 randWord words[30];
+
+//function prototypes for setWords and playGame
 void setWords();
 void playGame(int, int, int, int, int);
 
 int main()
 {
+	//tracker that allows the user to know the game is starting up
 	std::cout << "Let's Play!" << std::endl;
+
+	//sets the list of words that can be in play
 	setWords();
 	
-	int draw = 0;
-	int prevI = -1;
+	//--------------------------------
+	int draw = 0; //the variable that will randomly pick the index
+	//will stop the game from using indexes it has already called in the last three games
+	int prevI = -1; 
 	int prevII = -2;
 	int prevIII = -3;
-	int i = 0;
+	// an initialized score for the function call
 	int score = 0;
-			
+	//----------------------------------
+	
+	//prevents a random number call from being the same as it was in the last three games
 	while (draw == prevI || draw == prevII || draw == prevIII)
 	{
 		draw = rand() % 29 + 0;
@@ -38,6 +50,7 @@ int main()
 		}
 	}
 	
+	//calls the game
 	playGame(draw, prevI, prevII, prevII, score);
 		
 	return 0;
@@ -46,65 +59,70 @@ int main()
 void playGame(int draw, int prevI, int prevII, int prevIII, int score)
 {
 	//************************
-	std::string wordChosen;
-	std::string guessWord = "";
+	std::string wordChosen; //the indexed word
+	std::string guessWord = ""; //the word the player guesses if they choose to risk it all
 	wordChosen = words[draw].word;
-	std::string mystery = wordChosen;
+
+	std::string mystery = wordChosen; // what will be displayed to the player
+	//the letter by letter guess input
 	char guess;
+	//the user's input for if they want to risk the game to guess the whole word
 	int risk = 0;
+	//iterates when the user wins or looses
 	int win = 0;
 	int lose = 0;
+	//a loop iterator that does not throw a warning if it is iterating by an undefined number (the length of a random word)
     size_t i = 0;
+	//asks the user if they want to play again
 	int continueGame;
+	//iterates down when the user gets a letter wrong until the user looses
 	int chances = 6;
+	//iterates if the letter guessed does not equal the letter indexed by the checker
 	int failCheck = 0;
-	int wrongGuess;
 	//*************************
 
-
+	//displays the games the player has won
 	std::cout << "Games Won: " << score << std::endl;
 	
 	while (i < wordChosen.length())
 	{
+		//sets the mystery word to a length of ascii characters equal in length to the word
 		mystery[i] = '?';
 		i++;
 	}
 
 	while (win != 1 && lose != 1)
 	{
-		wrongGuess = 0;
-		std::cout << wordChosen/*mystery*/ << std::endl;
-		std::cout << "You have " << chances << " guesses left." << std::endl;
+		failCheck = 0;
+		//displays the mystery characters (for test purposes it displays the word right now)
+		std::cout << wordChosen + mystery << std::endl;
+		//displays the users remaining chances and asks what they would like to do
+		std::cout << "You have " << chances + 1 << " guesses left." << std::endl;
 		std::cout << "Would you like to guess a letter or try to guess the word?" << std::endl;
 		std::cout << "(1: guess a letter, 2: guess the word): ";
-		std::cin >> risk;
-		while (risk != 1 && risk != 2)
-		{
-			if (risk != 1 && risk != 2) 
-			{
-				std::cout << "That was not an acceptable option please choose one of the given options." << std::endl;
-				std::cout << "(1: guess a letter, 2: guess the word): ";
-				std::cin >> risk;
-			}
-			
-		}
+		std::cin >> risk; // user decides if they want to guess a word or letter
+	
 		switch (risk) //lets the user choose between playing normally, or risking the game on trying to guess the whole word
 		{
 		case 1: //Plays game as usual
 			std::cout << "Guess a letter: ";
 			std::cin >> guess;
+			//compares the user's guess to the characters in the string
 			for (size_t j = 0; j < mystery.length(); j++)
 			{
-				if (guess == mystery[j])
+				if (wordChosen[j] == guess)
 				{
 					mystery[j] = guess;
+					//the letter the user guessed is equal to the index
 				}
 
 				else
 				{
 					failCheck++;
+					//the letter the user guessed does not equal the index
 				}
 			}
+			//win, lose, continue conditions
 			if (mystery == wordChosen)//if the user has completed the word
 			{
 				std::cout << "Correct!  The word was " << wordChosen << ". " << std::endl;
@@ -114,22 +132,47 @@ void playGame(int draw, int prevI, int prevII, int prevIII, int score)
 			else if (failCheck == mystery.length())//if the user guessed a wrong letter
 			{
 				std::cout << "That was not a correct letter." << std::endl;
-				chances--;
-
-				switch (chances)
+				if (chances == 6)
 				{
-				case 3:// if the user is struggling
+					chances--;
+					break;
+				}
+				else if (chances == 5)
+				{
+					chances--;
+					break;
+				}
+				else if (chances == 4)
+				{
+					chances--;
+					break;
+				}
+				else if (chances == 3)
+				{
 					std::cout << "You seem to be having trouble, here's a hint!" << std::endl;
 					std::cout << words[draw].hint << std::endl;
+					chances--;
 					break;
-
-				case -1:// if the user lost
+				}
+				else if (chances == 2)
+				{
+					chances--;
+					break;
+				}
+				else if (chances == 1)
+				{
+					chances--;
+					break;
+				}
+				else if (chances == 0)
+				{
+					chances--;
+					break;
+				}
+				else if (chances == -1) // if the user lost
+				{
 					std::cout << "Sorry, but the correct word was " << wordChosen << std::endl;
 					lose++;
-					break;
-				default:
-					std::cout << "Place holder statement." << std::endl;
-					break;
 				}
 			}
 			break;
@@ -138,29 +181,31 @@ void playGame(int draw, int prevI, int prevII, int prevIII, int score)
 			std::cin >> guessWord;
 			if (guessWord == wordChosen)
 			{
+				// the player gussed the correct word
 				std::cout << "That is correct!  You Win!" << std::endl;
 				score++;
 				win++;
 			}
 			else
 			{
+				//the user guessed incorrectly
 				std::cout << "Nope!  You Lose!" << std::endl;
 				lose++;
 			}
 			break;
-		default:
+		default: //prevents the user from choosing outside the given options
 			std::cout << "I am sorry, this was not one of the given choices." << std::endl;
 			break;
 		}
-		
-		
-
 	}
-
+	//asks the user if they want to play again
 	std::cout << "Would you like to play again (1 to play again, 2 to end the game)?" << std::endl;
 	std::cout << "Play again: ";
 	std::cin >> continueGame;
 
+
+	//if the user wants to play again, the randomization process from int main() is repeated with this games index stored back one
+	// and the game before that moved back one and so on and so forth.
 	if (continueGame == 1)
 	{
 		prevIII = prevII;
@@ -174,10 +219,10 @@ void playGame(int draw, int prevI, int prevII, int prevIII, int score)
 				draw = rand() % 29 + 0;
 			}
 		}
-
+		//loops the game via recursion
 		playGame(draw, prevI, prevII, prevIII, score);
 	}
-
+	//quits the game
 	else
 	{
 		std::cout << "okay, thanks for playing!" << std::endl;
